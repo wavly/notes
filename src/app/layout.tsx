572585ff35b12815bc5a6b0e9e47958f14cee9ca
RoboXGamer <roboxgamer52@gmail.com>
2023-11-29 17:22:34 +0530

    Fixed:Main Landing Page minor style issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,9 @@ import type { Metadata } from "next";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
-
-
 export const metadata: Metadata = {
-  title: "Wavly",
-  description: "The Note taking App",
+	title: "Wavly",
+	description: "The Note taking App",
 };
 
 export default function RootLayout({
